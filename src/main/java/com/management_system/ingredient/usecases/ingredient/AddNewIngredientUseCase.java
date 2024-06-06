bc35fe1e1d1c55f9345a9a6d89eb1fdfbb9d42ef
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-07 00:28:00 +0700

    finish add new ingredient API

--- src/main/java/com/management_system/ingredient/usecases/ingredient/AddNewIngredientUseCase.java
+++ src/main/java/com/management_system/ingredient/usecases/ingredient/AddNewIngredientUseCase.java
@@ -1,15 +1,20 @@
 package com.management_system.ingredient.usecases.ingredient;
 
 import com.management_system.ingredient.entities.database.Ingredient;
+import com.management_system.ingredient.infrastucture.constant.IngredientStatusEnum;
 import com.management_system.ingredient.infrastucture.repository.IngredientRepository;
 import com.management_system.utilities.core.usecase.UseCase;
 import com.management_system.utilities.entities.ApiResponse;
 import com.management_system.utilities.entities.TokenInfo;
 import com.management_system.utilities.utils.JwtUtils;
+import com.management_system.utilities.utils.ValueParsingUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class AddNewIngredientUseCase extends UseCase<AddNewIngredientUseCase.InputValue, ApiResponse> {
@@ -19,12 +24,31 @@ public class AddNewIngredientUseCase extends UseCase<AddNewIngredientUseCase.Inp
     @Autowired
     JwtUtils jwtUtils;
 
+    @Autowired
+    ValueParsingUtils valueParsingUtils;
+
 
     @Override
     public ApiResponse execute(InputValue input) {
         try {
             TokenInfo tokenInfo = jwtUtils.getTokenInfoFromHttpRequest(input.request());
+            Ingredient newIngredient = input.ingredient();
 
+            Date currentTime = new Date();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+            String strDate = formatter.format(currentTime);
+
+            String formatedDateStr = valueParsingUtils.parseStringToId(strDate, "-", false);
+            String formatedIngredientName = valueParsingUtils.parseStringToId(newIngredient.getName(), "-", false);
+            String ingredientId = formatedIngredientName + "_" + formatedDateStr;
+
+            newIngredient.setId(ingredientId);
+            newIngredient.setCreationDate(currentTime);
+            newIngredient.setStatus(IngredientStatusEnum.AVAILABLE);
+            newIngredient.setLastUpdateUsername(tokenInfo.getUserName());
+
+            ingredientRepo.save(newIngredient);
 
             return ApiResponse.builder()
                     .result("success")
